9cde143a587cc26649b1969730bd351134db0506
Ash Feijoo <AshleyFeijoo1@gmail.com>
2019-06-21 14:15:29 -0500

    Updates

---
+++ precache-manifest.a819e4d59c312a45ead82895d89e37ce.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa64e823d312a893b69de8bb4a7fe97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d211629b2deff54340b7",
+    "url": "/static/css/main.f5b3b335.chunk.css"
+  },
+  {
+    "revision": "5830529018b74329100b",
+    "url": "/static/js/2.ba97f197.chunk.js"
+  },
+  {
+    "revision": "d211629b2deff54340b7",
+    "url": "/static/js/main.cde18028.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7e40b1c99603496a051e662c04b8f6c5",
+    "url": "/static/media/CuShj4j.7e40b1c9.jpg"
+  },
+  {
+    "revision": "8ce4c3fa166ccaafa6a06bd30e6c3497",
+    "url": "/static/media/Fergus.8ce4c3fa.jpeg"
+  },
+  {
+    "revision": "0088e1a8ebf24d634c3a8ef9dedd41ed",
+    "url": "/static/media/Fiona.0088e1a8.jpeg"
+  },
+  {
+    "revision": "9c6b829116ce69c3c30a90010bc5c04e",
+    "url": "/static/media/LordFarquad.9c6b8291.jpeg"
+  },
+  {
+    "revision": "64f8ddd9e8b6162ea218bb1179b8b4ec",
+    "url": "/static/media/Pinnochio.64f8ddd9.png"
+  },
+  {
+    "revision": "d5ad5f8c23de195e7ce5b9048c7eecd1",
+    "url": "/static/media/PrinceCharming.d5ad5f8c.jpeg"
+  },
+  {
+    "revision": "1a50c396689602d8f014345101e8184b",
+    "url": "/static/media/PussInBoots.1a50c396.jpeg"
+  },
+  {
+    "revision": "2179845ee578cd3bb30b55dc922bfb38",
+    "url": "/static/media/Shrek.2179845e.ttf"
+  },
+  {
+    "revision": "b0896ef3524d016879e93da9e0cc920a",
+    "url": "/static/media/Shrek.b0896ef3.jpeg"
+  },
+  {
+    "revision": "602f2f9a4f9aacf08a84897ef8aae334",
+    "url": "/static/media/ShrekMemory.602f2f9a.png"
+  },
+  {
+    "revision": "a7c190543d3e8ebc8a67d5aa4c4811ab",
+    "url": "/static/media/desc.a7c19054.png"
+  },
+  {
+    "revision": "e3c29f19995d05aecb469dabb2741a50",
+    "url": "/static/media/donkey.e3c29f19.jpeg"
+  },
+  {
+    "revision": "66401176dfd0e53cf16221becbd624e6",
+    "url": "/static/media/humanShrek.66401176.jpg"
+  },
+  {
+    "revision": "8d5ee2f7942fb326e62b15bb499611ce",
+    "url": "/static/media/shrekHeader.8d5ee2f7.png"
+  }
+]);
